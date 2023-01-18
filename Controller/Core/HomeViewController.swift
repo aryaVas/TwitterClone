884f512317055e3072120fea0e492316a38965a8
aryaVas <121014920+aryaVas@users.noreply.github.com>
2023-01-18 22:44:59 +0530

    Add files via upload

---
+++ Controller/Core/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  TwitterCloneUIkit
+//
+//  Created by Arya Vashisht on 29/11/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Controller/Core/MainTabBarViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TwitterCloneUIkit
+//
+//  Created by Arya Vashisht on 29/11/22.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+}
+
+extension MainTabBarController {
+    func initialiseNavStacks() {
+        
+    }
+}
